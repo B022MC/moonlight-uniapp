218b6010a979a483db0422bc3330bfcdb71772ab
B022MC <B022MC@outlook.com>
2023-08-21 03:05:42 +0800

    first commit

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,42 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+	<swiper class="banner" indicator-dots circular autoplay>
+		<swiper-item v-for="item in pictures" :key="item.id">
+			<image @tap="onPreviewImage(item.url)" mode="heightFix" :src="item.url"></image>
+		</swiper-item>
+	</swiper>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				pictures: [
+					{id: '1',url: "https://i.loli.net/2021/12/02/ITgAqBtKcO3RwPV.png"},
+					{id: '2',url: "https://i.loli.net/2021/12/02/KLh9n4FYQ1fpXmI.png"},
+					{id: '3',url: "https://i.loli.net/2021/11/26/atjlCUKQnY2AOqR.png"}
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			onPreviewImage(url){
+				//console.log(url)
+				wx.previewImage({
+					urls: this.pictures.map(v=>v.url),
+					current:url
+				})
+			}
+		}
+	}
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+	.banner,
+	.banner image{
+		width: 750rpx;
+		height: 500rpx;
+	}
 </style>
